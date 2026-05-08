2236db7f13d31c52516a6e473b71672d93d22337
harsu1207 <ht12072007@gmail.com>
2026-05-08 20:15:04 +0530

    Varibale and database type

---
+++ 02_variable_database.py
@@ -0,0 +1,29 @@
+# some types of varibale 
+
+
+a= 31
+print(type(a)) # Integer
+
+b= "Harshit"
+print(type(b)) #string
+
+c= 25.38
+print(type(c)) #floating
+
+d = True or False
+print(type(d)) #booleans
+
+e =[1,2,3,4,5,6]
+print(type(e)) #list
+
+f =("harshit", "yash ") 
+print(type(f)) #Tuple
+
+g= {1,2,3}
+print(type(g)) # set
+
+h= {"name": "Harshit", "my age": 19}
+print(type(h)) # Dictionary
+
+i= 2+3j
+print(type(i)) # complex
